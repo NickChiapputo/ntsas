1fd20a79cf98fe6785003796f5d061a63505a2ac
NickChiapputo <chiapputo.nick@gmail.com>
2021-02-02 22:38:28 -0600

    Added redirection for the /join/ path to go to the most occupied, non-empty room being watched.

--- node/server/index.js
+++ node/server/index.js
@@ -34,6 +34,49 @@ var serverAction = async function ( req, res )
 	console.log( " \n \n(" + date  + "): Redirect Query." );
 
 
+	// Get the path information.
+	let url_parts = url.parse( req.url );
+	let pathname = url_parts.pathname;
+
+
+	console.log( `Pathname: ${pathname}` );
+	// If we receive the join link, auto-route.
+	if( pathname === "/join/" )
+	{
+		// Get all the hubs rooms.
+		let query = {
+			watching: 1
+		};
+		let options = {};
+		let occupancyData = await dbLib.getHubsRooms( query, options );
+
+
+		// Iterate through each room and find the room
+		// with the highest occupancy under the threshold.
+		let redir_url = undefined;
+		let max_occ = -1;
+		for( let i = 0; i < occupancyData.length; i++ )
+		{
+			let curr_room = occupancyData[ i ];
+			if( curr_room.users < curr_room.threshold && curr_room.users > max_occ )
+			{
+				redir_url = curr_room.url;
+				max_occ = curr_room.users;
+			}
+		}
+
+		console.log( `Redirecting to ${redir_url}` );
+
+
+		// Redirect to the Hubs room.
+		res.writeHead( 302, {
+			'Location': redir_url
+		});
+		res.end();
+		return;
+	}
+
+
 	res.statusCode = 200;									// Set 200 OK status
 	res.setHeader( 'Content-Type', 'application/json' );	// Set response type as plain text since that's what we receive from the client.
 	res.setHeader( 'Access-Control-Allow-Origin', '*' );	// Allow CORS requests for local work
@@ -48,7 +91,7 @@ var serverAction = async function ( req, res )
 	};
 
 	let occupancyData = await dbLib.getHubsRooms( query, options );
-	console.log( `Return Vaule: ${JSON.stringify( occupancyData )}`);
+	// console.log( `Return Vaule: ${JSON.stringify( occupancyData )}`);
 
 
 	res.end( JSON.stringify( occupancyData ) );
